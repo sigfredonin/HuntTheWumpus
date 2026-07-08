4f8ff324c237cd5a1645e6be1af79cc6847bdcb9
Sigfredo Nin <signin@email.com>
2026-07-07 17:59:44 -0700

    Swap order of arrow hit something tests in fly_arrow().

--- Wumpus.pas
+++ Wumpus.pas
@@ -685,7 +685,7 @@ procedure fly_arrow ();
           write(arrow);
           { OK, what happens when the arrow reaches this room? 
             These tests may set player state to WON or LOST. }
-          if not(did_arrow_hit_hunter(arrow) or did_arrow_hit_wumpus(arrow)) then
+          if not(did_arrow_hit_wumpus(arrow) or did_arrow_hit_hunter(arrow)) then
             { Arrow did not hit anything, try next room in path }
             roomIndex := roomIndex + 1
         until (roomIndex > arrow_path_length) or not (player = IN_PLAY)
